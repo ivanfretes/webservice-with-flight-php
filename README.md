02c4f573509727ebf6925f10c3b85453703fd57f
Iván Fretes <vanfretes@gmail.com>
2017-07-28 20:09:38 -0400

    Basic config

--- README.md
+++ README.md
@@ -2,18 +2,17 @@
 # webservice with fligth php 
 A form to create a simple and no complicate webservice in minutes 
 
-###Requeriments?:
+### Requeriments?: ###
 
-
-###Features
+### Features ###
 
 * foldername : It's optional
 * webservice : To you like name, this is a example, the name can be any
 * table : It's a name of table of the database, I suggest add a prefix/postfix or encapsulate it and validate in the code
 * field_name : (Optional) Generaly this is a identify(id) or code of the database
-* field_value : It's required if you initialize the field_value
+* field_value : It's required if you initialize the field_name
 
-# How to Implement?:
+### How to Implement?: ###
 
 ---
 
@@ -25,6 +24,17 @@ A form to create a simple and no complicate webservice in minutes
 
 ## Do you want a create token?
 
+Modify the code in the index.php file
 
+	<?
+
+	define("DBMANAGER", "yourdbmanager"); // eg mysql, postgre, cubrid, etc
+	define("URL_WEBSERVICE", "yoururlbase");
+	define("HOSTNAME","localhost");
+	define("DBNAME","yourdbname");
+	define("HOSTUSER","yourdbuser");
+	define("HOSTPASS", "yourdbpassword");
+
+	<?
 
 Fligth Documentation - http://flightphp.com/learn
--- index.php
+++ index.php
@@ -11,22 +11,22 @@ require 'flight/Flight.php';
 
 
 /**
- * Atributos de conexion a la base de datos
+ * Atributos de conexion a la base de datos, 
  */
 
 define("DBMANAGER", "mysql");
 define("URL_WEBSERVICE", "/");
 define("HOSTNAME","localhost");
-define("DBNAME","restaurant");
+define("DBNAME","dbname");
 define("HOSTUSER","root");
-define("HOSTPASS", "admin123");
+define("HOSTPASS", "123456");
 	
 
 /**
  * Registramos atributo db como propiedad de Flight
  */
 Flight::register('db', 'PDO', array( 
-								DBMANAGER':host='.HOSTNAME.';dbname='.DBNAME ,
+								DBMANAGER.':host='.HOSTNAME.';dbname='.DBNAME ,
 								HOSTUSER, HOSTPASS ), 
 	function($db){
 	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -34,113 +34,138 @@ Flight::register('db', 'PDO', array(
 	}
 );
 
+
+
+
 /**
  * Listado de registros
  * @example  ~/facturacion
  * 
  * Lista un registro
- * @example  ~/facturacion/factura_id/265
+ * @example  ~/facturacion/facturaid/265
  * 
- * @param {number} @table : Nombre de la tabla
- * @param {@field_value} Coincidencia buscada
+ * @param number @table : Nombre de la tabla
+ * @param string $field_name _ Nombre del campo a relacionar
+ * @param mixed $field_value : Valor buscado en el $field_name
  */
-Flight::route("GET /webservice/@table(/@field_name(/@field_id))", 
-	function($table,$param1,$param2) {
-	$db = Flight::db();
+Flight::route("GET /@table(/@field_name(/@field_value))", 
+	function($table, $field_name, $field_value) {
+
+	if (Flight::verifyNull($table)){
 
 
-	$query = '';
-	if (isset($field_name) && isset($field_value)){
+		$db = Flight::db();
+		$query = " SELECT * FROM $table ";
 
-		$query = "SELECT * 
-    			  FROM $table 
-    			  "
-		// Retorna datos por un campo en especifico
-		else if (isset($param2)){				
-			$keyName = Flight::setFieldKeyName($param1,"",false);
-			$query = "WHERE $keyName = $param2"; 
-		}	
+		// Si los datos del registro estan inicializados
+		if (is_string($field_name) && isset($field_value)){
+			$query = " WHERE $field_name = $field_value "; 
+		}
+
+		// Desde y cantidad de registro a visualizar
+		$query .= Flight::limitRow(Flight::request()->query['init'],
+								   Flight::request()->query['cant']);
+
+  
+	    $stmt = $db->prepare($query);
+	    $stmt->execute();
+
+	    Flight::headerData();
+		echo json_encode($stmt->fetchAll(PDO::FETCH_CLASS));
+
+		$db = null;	 
 	}
-	else {
-
-	}
-
-	$query .= Flight::limitRow();
-
-
-    $stmt = $db->prepare(.$query);
-    
-    $stmt->execute();
-
-    header('Content-type: application/json');
-	echo json_encode($stmt->fetchAll(PDO::FETCH_CLASS));
-
 	
 
-	$db = null;	 
 });
 
 
 /**
- * Inserta en las rutas ~/tabla/field_id 
+ * Listado de registros
+ * @example  ~/facturacion
  * 
- * @param {number} @table : Nombre de la tabla
- * @param {@field_name} Nombre del campo 
- * @param {@field_value} Coincidencia buscada
+ * Lista un registro
+ * @example  ~/facturacion/facturaid/265
+ * 
+ * @param number @table : Nombre de la tabla
+ * @param string $field_name _ Nombre del campo a relacionar
+ * @param mixed $field_value : Valor buscado en el $field_name
  */
-// Flight::route("POST /webservice/@table(/@field_name(/@field_value))", 
-// 	function($table,$param1,$param2) {
-	
-// 	$db = Flight::db();
+Flight::route("GET /@table(/@field_name(/@field_value))", 
+	function($table, $field_name, $field_value) {
 
-// 	if (Flight::request()->method == "GET"){
-// 		$query = '';	
+	if (Flight::verifyNull($table)){
 
-// 		if (isset($param1)){
-// 			// Retorna datos por id
-// 			if (is_numeric($param1) && !isset($param2)){
-// 				$keyName = Flight::setFieldKeyName($table,"id_",false);
-// 				$query = "WHERE $keyName = $param1";
-// 			}
-// 			// Retorna datos por un campo en especifico
-// 			else if (isset($param2)){				
-// 				$keyName = Flight::setFieldKeyName($param1,"",false);
-// 				$query = "WHERE $keyName = $param2"; 
-// 			}	
-// 		}
 
-// 		$query .= Flight::limitRow($_GET['init'], $_GET[]);
+		$db = Flight::db();
+		$query = " SELECT * FROM $table ";
 
-	
-// 	    $stmt = $db->prepare("SELECT * 
-// 	    					  FROM $table ".$query);
-	    
-// 	    $stmt->execute();
+		// Si los datos del registro estan inicializados
+		if (is_string($field_name) && isset($field_value)){
+			$query = " WHERE $field_name = $field_value "; 
+		}
 
-// 	    header('Content-type: application/json');
-// 		echo json_encode($stmt->fetchAll(PDO::FETCH_CLASS));
+		// Desde y cantidad de registro a visualizar
+		$query .= Flight::limitRow(Flight::request()->query['init'],
+								   Flight::request()->query['cant']);
 
-// 	}
+  
+	    $stmt = $db->prepare($query);
+	    $stmt->execute();
+
+	    Flight::headerData();
+		echo json_encode($stmt->fetchAll(PDO::FETCH_CLASS));
+
+		$db = null;	 
+	}
 	
 
-// 	$db = null;	 
-// });
+});
 
 
-// Verifica si las variables init, y limit estan inicializas, en dicho caso, 
-// retorna el query necesario 
-Flight::map('limitRow', function($init, $limit){
-	$query = '';
+/**
+ * Generación de Index del webservice
+ */
+Flight::route('/', function(){
+	echo "hello word. Webservice made with Flight";
+});
+
+
+
+
+/**
+ * Verifica todos los posibles valores, nulos
+ * 0, NULL, FALSE , 
+ */
+Flight::map('verifyNull', function($word){
+	if ('' === trim($word) || NULL === $word || 
+		0 == $word){
+		return TRUE;
+	}
+
+	return FALSE;
+});
+
+
+/**
+ * Genera el limite y la cantidad de registros 
+ * 
+ * 
+ * @param number $init : Registro Inicial
+ * @param number $limit : Cantidad de registros
+ * 
+ */
+Flight::map('limitRow', function($init, $cant){
 
 	if (NULL === $init)	$init = 0;
-	if (NULL === $limit) $limit = 25;
+	if (NULL === $cant) $cant = 25;
 
 	try {
 		
-		if (!is_numeric($init) || !is_numeric($limit))
-			throw new Exception("Valores del init o limit son incorrectos", 1);
+		if (!is_numeric($init) || !is_numeric($cant))
+			throw new Exception("Valores del init o cant son incorrectos");
 			  
-		$query = " LIMIT $init, $limit";
+		return " LIMIT $init, $cant ";
 
 	} catch (Exception $e) {
 		echo $e->getMessage();
@@ -148,28 +173,19 @@ Flight::map('limitRow', function($init, $limit){
 });
 
 
-// Genera los queries del buscador 
-Flight::map('setValueFilterSearch', function($getParam,$field,
-											 $boolean = "="){
-	$queries = "";
-
-	if (isset($getParam)){
-		$queries = " $field $boolean $getParam";
-		return $queries;
-	}
-	return "";
-
-});
-
 
 /**
  * Verificamos la procedencia de los accesos
  */
-Flight::map('controlOrigin', function($permiso = '*'){
-	header('Access-Control-Allow-Origin: $permiso'); 	
+Flight::map('headerData', function(){
+	header('Access-Control-Allow-Origin: *'); 
+	header('Content-type: application/json');	
 });
 
-Flight::controlOrigin();
+
+/**
+ * Inicializamos el webservice
+ */
 Flight::start();
 
 ?>
